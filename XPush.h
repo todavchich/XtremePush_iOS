6610fa913af1600f45e3a3207baf016cf7a18e3b
Slava Todavchich <slava@moqod.com>
2013-10-24 12:14:29 +0300

    added a notification for successful registration

--- XPush.h
+++ XPush.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
+extern NSString *const XPushDeviceRegistrationNotification;
+
 @interface XPush : NSObject
 
 + (BOOL)isSandboxModeOn;
@@ -27,5 +29,25 @@
 + (NSDictionary *)deviceInfo;
 
 + (void)hitTag:(NSString *)tag;
++ (void)getPushNotificationsFromDate:(NSDate *)date completion:(void(^)(NSArray *pushList, NSError *error))completion;
++ (void)showPushListController;
 
 @end
+
+
+@interface XPPushModel : NSObject
+
+@property (nonatomic, readonly) NSDate      *createDate;
+@property (nonatomic, readonly) NSString    *pushId;
+@property (nonatomic, readonly) NSString    *locationId;
+@property (nonatomic, readonly) NSString    *alert;
+@property (nonatomic, readonly) NSInteger   badge;
+@property (nonatomic, readonly) NSString    *messageId;
+@property (nonatomic, readonly) NSString    *url;
+@property (nonatomic, readonly) BOOL        shouldOpenInApp;
+
+- (id)initWithDictionary:(NSDictionary *)dictionary;
+
+@end
+
+
